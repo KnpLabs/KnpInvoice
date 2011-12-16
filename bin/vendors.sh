14bf4da63a45543cf71c2799839d3d65ed674e38
Matthieu Bontemps <matthieu.bontemps@knplabs.com>
2011-12-16 13:08:41 +0100

    Update the vendors script to be coherent with other bundles

--- bin/vendors.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-git clone https://github.com/symfony/ClassLoader.git vendor/symfony-class-loader
-git clone https://github.com/fabpot/Twig.git vendor/twig
-git clone https://github.com/KnpLabs/snappy.git vendor/knp/snappy
